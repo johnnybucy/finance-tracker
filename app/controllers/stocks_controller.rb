963bca09eda909e0080315ee8249a1255917b315
Ubuntu <ubuntu@ip-10-0-0-135.us-west-2.compute.internal>
2020-10-21 18:03:55 +0000

    new stock form works, can add a category to a real stock!

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -21,17 +21,25 @@ class StocksController < ApplicationController
   end
   
   def new
-    @stock = Stock.new
+      tick = params[:ticker]
+        tick = tick.downcase
+        @stock = Stock.check_db(tick)
+        if @stock.blank?
+            @stock = Stock.new_lookup(tick)
+        end
   end
   
   def create
-    @stock = Stock.new
+    stock = Stock.new
     puts "TICKER =" + params[:stock][:ticker]
-    @stock.name =  params[:stock][:name]
-    @stock.ticker =  params[:stock][:ticker]
-    @stock.last_price =  params[:stock][:latest_price]
-    @stock.category =  params[:stock][:category]
-    @stock.save
+    stock.name =  params[:stock][:name]
+    stock.ticker =  params[:stock][:ticker]
+    stock.last_price =  params[:stock][:last_price]
+    stock.category =  params[:stock][:category]
+    stock.save
+    @user_stock = UserStock.create(user: current_user, stock: stock)
       redirect_to my_portfolio_path 
   end
 end
+  
+        
